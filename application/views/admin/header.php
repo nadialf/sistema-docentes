877c21623d57118c2e1f520d3ac2b01318f9455b
Nadia Libreros <nadialbrs@hotmail.com>
2016-05-17 12:53:08 -0500

    Logo SAC

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -24,12 +24,9 @@
   <nav class="navbar navbar-default"> <!-- navbar-fixed-top" role="navigation"> -->
     <div class="container-fluid">
       <div class="navbar-header" style="margin:5px;">
-        <img src="<?php echo base_url(); ?>assets/img/LogoFEI.png">
+        <img src="<?php echo base_url(); ?>assets/img/LogoSAC.png">
       </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav" style="margin:5px;">
-            <li><a style="color:#FFF; font-size:45px;"> SAC</a></li>
-          </ul>
           <ul class="nav navbar-nav navbar-right">
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
--- application/views/login_view.php
+++ application/views/login_view.php
@@ -26,7 +26,7 @@
 <nav class="navbar navbar-default"> <!-- navbar-fixed-top" role="navigation"> -->
   	<div class="container-fluid">
   		<div class="navbar-header" style="margin:5px;">
-  			<img src="assets/img/LogoFEI.png">
+  			<img src="assets/img/LogoSAC.png">
   		</div>
   		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
     		<ul class="nav navbar-nav" style="margin:5px;">
